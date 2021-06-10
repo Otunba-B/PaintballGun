94e75d0da0dec561c9bf3c2397e3a898f9c65c89
Otunba-B <bryanejiofor@ymail.com>
2021-06-10 16:08:55 +0100

    Add project files.

---
+++ PaintballGun.cs
@@ -0,0 +1,44 @@
+﻿
+
+using System;
+
+namespace Paintball
+{
+    class PaintballGun
+    {
+        public const int Magazine_Size = 16;
+
+        private int balls = 0;
+        private int ballsloaded = 0;
+
+        public int GetBallsLoaded() { return ballsloaded; }
+        public bool isEmpty() { return ballsloaded == 0; }
+
+        public int Balls
+        {
+            get { return balls; }
+
+            set
+            {
+                if (value > 0)
+                    balls = value;
+                Reload();
+            }
+        }
+        public void Reload()
+        {
+            if (balls > Magazine_Size)
+                ballsloaded = Magazine_Size;
+            else
+                ballsloaded = balls;
+        }
+
+        public bool Shoot()
+        {
+            if( ballsloaded == 0) { return false; }
+            ballsloaded--;
+            balls--;
+            return true;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Paintball
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PaintballGun gun = new PaintballGun();
+            while (true)
+            {
+                Console.WriteLine($"{gun.Balls} balls, {gun.GetBallsLoaded()} loaded balls");
+
+                if (gun.isEmpty())
+                    Console.WriteLine(" WARNING: You are out of ammo. ");
+
+                Console.WriteLine("Space to shoot, r to reload, + to add ammo, q to quit");
+                char key = Console.ReadKey(true).KeyChar;
+
+                if (key == ' ')
+                    Console.WriteLine($"Shooting returned {gun.Shoot()}");
+                else if (key == 'r')
+                    gun.Reload();
+                else if (key == '+')
+                    gun.Balls += PaintballGun.Magazine_Size;
+                else if (key == 'q')
+                    return;
+            }
+        }
+    }
+}
